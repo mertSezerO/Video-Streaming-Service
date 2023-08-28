2d628100410afa08408f9ed1dc00a895c8e0e764
mertSezerO <mertS.Oktay@gmail.com>
2023-08-28 17:04:02 +0300

    Searching debugging done.

--- frontend/src/components/searchBar.js
+++ frontend/src/components/searchBar.js
@@ -4,11 +4,9 @@ import { SearchContext } from "../contexts/searchContext";
 export default function SearchBar() {
   const context = useContext(SearchContext);
 
-  function filterMovies(e) {
-    const searchTerm = e.target.value;
-    context.setSearchValue(searchTerm);
+  function getMovies() {
     if (!context.filteredMovies) {
-      fetch("http://localhost:5000/movies?search=" + searchTerm, {
+      fetch("http://localhost:5000/movies?search=", {
         headers: {
           "Content-Type": "application/json",
         },
@@ -20,13 +18,27 @@ export default function SearchBar() {
     }
   }
 
+  function filterMovies(e) {
+    const searchTerm = e.target.value;
+    context.setSearchValue(searchTerm);
+    fetch("http://localhost:5000/movies?search=" + searchTerm, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((movies) =>
+      movies.json().then(({ movies }) => {
+        context.setFilteredMovies(movies);
+      })
+    );
+  }
+
   return (
     <input
       type="text"
       placeholder="Search a movie, genre or a keyword"
       value={context.searchValue}
       onChange={filterMovies}
-      onFocus={filterMovies}
+      onFocus={getMovies}
     />
   );
 }
