f92ca4a2bfaa13ceb0c0795a2a36ff3f7eb95f60
mertSezerO <mertS.Oktay@gmail.com>
2023-08-28 16:33:41 +0300

    Search page done.

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,5 +1,10 @@
 import HomeProvider from "../contexts/homeContext";
 
 export default function HomePage() {
-  return <HomeProvider></HomeProvider>;
+  return (
+    <HomeProvider>
+      <div id="recent-item-container"></div>
+      <div id="item-list-container"></div>
+    </HomeProvider>
+  );
 }
--- frontend/src/pages/search.js
+++ frontend/src/pages/search.js
@@ -1,5 +1,20 @@
-import SearchProvider from "../contexts/searchContext";
+import { useContext } from "react";
+
+import SearchBar from "../components/searchBar";
+import SearchProvider, { searchContext } from "../contexts/searchContext";
+import MovieItem from "../components/movieItem";
 
 export default function SearchPage() {
-  return <SearchProvider></SearchProvider>;
+  const context = useContext(searchContext);
+  return (
+    <SearchProvider>
+      <div id="filter-container">
+        <SearchBar />
+      </div>
+      <div id="search-item-container">
+        {context.filteredMovies &&
+          context.filteredMovies.map((movie) => <MovieItem movie={movie} />)}
+      </div>
+    </SearchProvider>
+  );
 }
